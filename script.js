461c527a063dbf8d699b226fa746ad2afd45ae09
Sheng232 <Shengj12345@gmail.com>
2024-10-23 10:44:34 -0700

    added a google search engine

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const events = document.querySelector(".event");
-const timeElement = events.querySelector(".time");
+const timeElement = document.querySelector(".time");
 const dateElement = document.querySelector(".date");
-const timeZone = document.querySelector("time-zone");
+const timeZone = document.querySelector(".time-zone");
+const searchBtn = document.querySelector(".gsc-search-button");
 
-
+// searchBtn.addEventListener("click", ()=>{
+//   events.style.display = "none";
+// });
 const updateDate = () => {
   const date = dayjs().format("dddd, DD, MMMM, YYYY");
   const time = dayjs().format("hh:mm:ss A");
-  // const zone = dayjs.tz.guess();
   timeElement.innerText = time;
   dateElement.innerText = date;
 };
