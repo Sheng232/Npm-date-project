0e48c0498f23233aae7ea9125fcd88c2a0575c5a
sheng232 <lowellchessofficial@gmail.com>
2024-10-23 01:35:17 -0700

    second commit

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const events = document.querySelector(".event");
+const timeElement = events.querySelector(".time");
+const dateElement = document.querySelector(".date");
+const timeZone = document.querySelector("time-zone");
+
 
 const updateDate = () => {
-  const currentDate = new Date();
-  events.setAttribute("data-date", `${currentDate}`);
-
-  const date = dayjs(events.dataset.date).format("MMM-DD-YYYY hh:mm:ss A");
-  const dateElement = events.querySelector(".date");
+  const date = dayjs().format("dddd, DD, MMMM, YYYY");
+  const time = dayjs().format("hh:mm:ss A");
+  // const zone = dayjs.tz.guess();
+  timeElement.innerText = time;
   dateElement.innerText = date;
 };
 
-
-updateDate();
 
 setInterval(updateDate, 1000);
