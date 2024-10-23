f3dabe9d490cc181db3927dc4fffdb7d18875f1b
sheng232 <lowellchessofficial@gmail.com>
2024-10-23 16:03:27 -0700

    added a time zone displayer

--- script.js
+++ script.js
@@ -4,15 +4,22 @@ const dateElement = document.querySelector(".date");
 const timeZone = document.querySelector(".time-zone");
 const searchBtn = document.querySelector(".gsc-search-button");
 
-// searchBtn.addEventListener("click", ()=>{
-//   events.style.display = "none";
-// });
 const updateDate = () => {
   const date = dayjs().format("dddd, DD, MMMM, YYYY");
   const time = dayjs().format("hh:mm:ss A");
   timeElement.innerText = time;
   dateElement.innerText = date;
+  timeZone.innerText = getTimezoneName();
 };
+
+function getTimezoneName() {
+  const today = new Date();
+  const options = {
+    timeZoneName: 'long'
+  };
+
+  return new Intl.DateTimeFormat('en-US', options).formatToParts(today).find(part => part.type === 'timeZoneName').value;
+}
 
 
 setInterval(updateDate, 1000);
